5025918a0ef7376531ac0877e6a9ee8cbd0f7af8
Kevin Jordan <kevinajordan@gmail.com>
2024-08-12 16:49:16 -0400

    updated to catch out of area airbnbs

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -25,6 +25,7 @@ def go(args):
     min_price = args.min_price
     max_price = args.max_price
     idx = df['price'].between(min_price, max_price)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
     df.to_csv("clean_sample.csv", index=False)
